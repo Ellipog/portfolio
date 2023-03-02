a40fcbab14f70f29dc32b92b9915495c2a7b0e96
Ellipog <elliotsaaen@hotmail.com>
2023-03-02 19:09:27 +0100

    Added personal window

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import styles from "./css/App.module.css";
 import React, { useState, useEffect } from "react";
 import startLogo from "./img/startLogo.png";
+import personal from "./img/personal.png";
+import shield from "./img/shield.png";
+import close from "./img/close.png";
+import elliotProfile from "./img/elliotProfile.jpg";
 
 function App() {
   const [clock, setClock] = useState(Date.now());
@@ -17,19 +21,34 @@ function App() {
   }, []);
 
   const handleProfileClick = () => {
-    console.log(profile);
     setProfile(!profile);
-    console.log(profile);
   };
 
   return (
     <div className={styles.container}>
-      <button onClick={handleProfileClick}>Click</button>
       <div className={styles.screen}>
         {profile ? (
-          <div className={styles.profile}>
-            <img src={""} alt="" />
-            <p>WHAHWHAWHAWHHW</p>
+          <div className={styles.profileContainer}>
+            <div className={styles.personalHeader}>
+              <img className={styles.personalIMG} src={personal} alt="Personal" />
+              <p>Elliot Strand Aaen</p>
+              <img className={styles.closeIMG} src={close} alt="Personal" onClick={() => handleProfileClick()} />
+            </div>
+            <div className={styles.profileInfoContainer}>
+              <div className={styles.profileIMG}>
+                <p>Elliot.png</p>
+                <img src={elliotProfile} />
+              </div>
+              <div className={styles.profileInfo}>
+                <p className={styles.profileAboutHD}>About:</p>
+                <p className={styles.profileAbout}>
+                  - Web Developer
+                  <br />- Programmer
+                  <br />- Student
+                  <br />- 17 years old
+                </p>
+              </div>
+            </div>
           </div>
         ) : null}
       </div>
@@ -38,8 +57,10 @@ function App() {
           <img src={startLogo} alt="WindowsXPLogo" />
           <p>START</p>
         </div>
+        <img className={styles.personalNavbar} src={personal} alt="Personal" onClick={() => handleProfileClick()} />
         <div className={styles.navBarRight}>
           <div className={styles.time}>
+            <img src={shield} />
             {hours}:{minutes}
           </div>
         </div>
