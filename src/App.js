86ded5aeb369a2977dabb453bf046d667622dc01
Ellipog <elliotsaaen@hotmail.com>
2023-03-02 17:12:17 +0100

    Button doesnt work, dont know why

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import styles from "./css/App.module.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import startLogo from "./img/startLogo.png";
 
 function App() {
   const [clock, setClock] = useState(Date.now());
+  const [profile, setProfile] = useState(true);
 
   let hours = ("0" + new Date(clock).getHours()).slice(-2);
   let minutes = ("0" + new Date(clock).getMinutes()).slice(-2);
 
-  setInterval(() => setClock(Date.now()), 100);
+  useEffect(() => {
+    const clockUpdater = setInterval(() => setClock(Date.now()), 1000);
+    return function cleanup() {
+      clearInterval(clockUpdater);
+    };
+  }, []);
+
+  const handleProfileClick = () => {
+    console.log(profile);
+    setProfile(!profile);
+    console.log(profile);
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.clock}>
-        <div className={styles.time}>{hours}:{minutes}</div>
+      <button onClick={handleProfileClick}>Click</button>
+      <div className={styles.screen}>
+        {profile ? (
+          <div className={styles.profile}>
+            <img src={""} alt="" />
+            <p>WHAHWHAWHAWHHW</p>
+          </div>
+        ) : null}
       </div>
-      <div className={styles.navBar}></div>
+      <div className={styles.navBar}>
+        <div className={styles.navBarLeft}>
+          <img src={startLogo} alt="WindowsXPLogo" />
+          <p>START</p>
+        </div>
+        <div className={styles.navBarRight}>
+          <div className={styles.time}>
+            {hours}:{minutes}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
