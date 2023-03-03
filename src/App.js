dbd757bd205bbedbe1dc5b8fb6f557cda7c7d0ef
Ellipog <45790498+Ellipog@users.noreply.github.com>
2023-03-03 13:00:58 +0100

    Draggable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styles from "./css/App.module.css";
 import React, { useState, useEffect } from "react";
+import Draggable from 'react-draggable';
 import startLogo from "./img/startLogo.png";
 import personal from "./img/personal.png";
 import experienceIMG from "./img/experience.png";
@@ -11,8 +12,10 @@ import elliotProfile from "./img/elliotProfile.jpg";
 function App() {
   const [clock, setClock] = useState(Date.now());
   const [profile, setProfile] = useState(true);
-  const [experience, setExperience] = useState(true);
-  const [education, setEducation] = useState(true);
+  const [experience, setExperience] = useState(false);
+  const [education, setEducation] = useState(false);
+  const [drag, setDrag] = useState("");
+  const [grab, setGrab] = useState("");
 
   let hours = ("0" + new Date(clock).getHours()).slice(-2);
   let minutes = ("0" + new Date(clock).getMinutes()).slice(-2);
@@ -22,34 +25,28 @@ function App() {
     return function cleanup() {
       clearInterval(clockUpdater);
     };
-  }, []);
-
-  const handleProfileClick = () => {
-    setProfile(!profile);
-  };
-
-  const handleExperienceClick = () => {
-    setExperience(!experience);
-  };
-
-  const handleEducationClick = () => {
-    setEducation(!education);
-  };
+  });
 
   return (
     <div className={styles.container}>
       <div className={styles.screen}>
+        <div className={styles.apps}>
+          <img className={`${styles.profileApp} ${styles.app}`} src={personal} alt="Personal" onClick={() => setProfile(!profile)} />
+          <img className={`${styles.experienceApp} ${styles.app}`} src={experienceIMG} alt="Experience" onClick={() => setExperience(!experience)} />
+          <img className={`${styles.educationApp} ${styles.app}`} src={educationIMG} alt="Education" onClick={() => setEducation(!education)} />
+        </div>
         {profile ? (
-          <div className={styles.profileContainer}>
+          <Draggable>
+          <div className={`${drag === "personal" ? styles.dragged : null} ${grab === "personal" ? styles.grab : null} ${styles.profileContainer}`} onMouseDownCapture={() => {setDrag("personal"); setGrab("personal");}} onMouseUpCapture={() => setGrab("")}>
             <div className={styles.personalHeader}>
               <img className={styles.personalIMG} src={personal} alt="Personal" />
               <p>Elliot Strand Aaen</p>
-              <img className={styles.personalCloseIMG} src={close} alt="Close" onClick={() => handleProfileClick()} />
+              <img className={styles.personalCloseIMG} src={close} alt="Close" onClick={() => setProfile(!profile)} />
             </div>
             <div className={styles.profileInfoContainer}>
               <div className={styles.profileIMG}>
                 <p>Elliot.png</p>
-                <img src={elliotProfile} />
+                <img src={elliotProfile} alt="Elliot.png" />
               </div>
               <div className={styles.profileInfo}>
                 <p className={styles.profileAboutHD}>About:</p>
@@ -62,13 +59,15 @@ function App() {
               </div>
             </div>
           </div>
+          </Draggable>
         ) : null}
         {experience ? (
-          <div className={styles.experienceContainer}>
+          <Draggable>
+          <div className={`${drag === "experience" ? styles.dragged : null} ${grab === "experience" ? styles.grab : null} ${styles.experienceContainer}`} onMouseDownCapture={() => {setDrag("experience"); setGrab("experience");}} onMouseUpCapture={() => setGrab("")}>
             <div className={styles.experienceHeader}>
               <img className={styles.experienceIMG} src={experienceIMG} alt="Experience" />
               <p>Experience</p>
-              <img className={styles.experienceCloseIMG} src={close} alt="Close" onClick={() => handleExperienceClick()} />
+              <img className={styles.experienceCloseIMG} src={close} alt="Close" onClick={() => setExperience(!experience)} />
             </div>
             <div className={styles.experienceInfoContainer}>
               <div className={styles.experienceInfo}>
@@ -90,34 +89,32 @@ function App() {
               </div>
             </div>
           </div>
+          </Draggable>
         ) : null}
         {education ? (
-          <div className={styles.educationContainer}>
+          <Draggable>
+          <div className={`${drag === "education" ? styles.dragged : null} ${grab === "education" ? styles.grab : null} ${styles.educationContainer}`} onMouseDownCapture={() => {setDrag("education"); setGrab("education");}} onMouseUpCapture={() => setGrab("")}>
             <div className={styles.educationHeader}>
               <img className={styles.educationIMG} src={educationIMG} alt="Education" />
               <p>Education</p>
-              <img className={styles.educationCloseIMG} src={close} alt="Close" onClick={() => handleEducationClick()} />
+              <img className={styles.educationCloseIMG} src={close} alt="Close" onClick={() => setEducation(!education)} />
             </div>
             <div className={styles.educationInfoContainer}>
               <div className={styles.educationInfo}>
                 <p className={styles.educationList}>
-                  - Caretaker <span className={styles.schoolName}>Greverud Sykehjem</span>
+                  - Year 1 Information Technology & Media<br/> <span className={styles.schoolName}>Drømtorp VGS</span>
                   <br />
-                  <span className={styles.educationTime}>2022 JUL - 2022 AUG</span>
+                  <span className={styles.educationTime}>2020 AUG - 2021 JUN</span>
+                  <br /><br/>
+                  - Year 2 Information Technology<br/> <span className={styles.schoolName}>Drømtorp VGS</span>
                   <br />
-                  <br />- Technical Support <span className={styles.schoolName}>Intility</span>
+                  <span className={styles.educationTime}>2021 AUG - NOW</span>
                   <br />
-                  <span className={styles.educationDesc}>Department: Setup</span>
-                  <br />
-                  <span className={styles.educationTime}>2022 OKT - 2022 NOV</span>
-                  <br />
-                  <br />- Caretaker <span className={styles.schoolName}>Greverud Sykehjem</span>
-                  <br />
-                  <span className={styles.educationTime}>2022 DEC - 2022 DEC</span>
                 </p>
               </div>
             </div>
           </div>
+          </Draggable>
         ) : null}
       </div>
       <div className={styles.navBar}>
@@ -126,13 +123,13 @@ function App() {
           <p>START</p>
         </div>
         <div className={styles.navbarElements}>
-          <img className={`${profile ? styles.activeNavbar : null} ${styles.personalNavbar}`} src={personal} alt="Personal" onClick={() => handleProfileClick()} />
-          <img className={`${experience ? styles.activeNavbar : null} ${styles.experienceNavbar}`} src={experienceIMG} alt="Experience" onClick={() => handleExperienceClick()} />
-          <img className={`${education ? styles.activeNavbar : null} ${styles.educationNavbar}`} src={educationIMG} alt="Education" onClick={() => handleEducationClick()} />
+          <img className={`${profile ? styles.activeNavbar : null} ${styles.personalNavbar}`} src={personal} alt="Personal" onClick={() => setProfile(!profile)} />
+          <img className={`${experience ? styles.activeNavbar : null} ${styles.experienceNavbar}`} src={experienceIMG} alt="Experience" onClick={() => setExperience(!experience)} />
+          <img className={`${education ? styles.activeNavbar : null} ${styles.educationNavbar}`} src={educationIMG} alt="Education" onClick={() => setEducation(!education)} />
         </div>
         <div className={styles.navBarRight}>
           <div className={styles.time}>
-            <img src={shield} />
+            <img src={shield} alt=""/>
             {hours}:{minutes}
           </div>
         </div>
